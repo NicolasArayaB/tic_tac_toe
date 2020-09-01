ec63bdf6210b2dca68e62808289e094cf0293199
NicolasArayaB <nicolasaraya@gmail.com>
2020-09-01 15:45:28 -0400

    done

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,6 @@
-$board = [["   ", "1", "  2", "  3"], ["A ", "[ ]", "[ ]", "[ ]"], 
-         ["B ", "[ ]", "[ ]", "[ ]"], ["C ", "[ ]", "[ ]", "[ ]"]]
-$tracker = [["a1" ,"b1" ,"c1" ], ["a2" ,"b2" ,"c2" ], ["a3" ,"b3" ,"c3" ]]
+$board = [[´   ´, ´1´, ´  2´, ´  3´], [´A ´, ´[ ]´, ´[ ]´, ´[ ]´], 
+         [´B ´, ´[ ]´, ´[ ]´, ´[ ]´], [´C ´, ´[ ]´, ´[ ]´, ´[ ]´]]
+$tracker = [[´a1´ ,´b1´ ,´c1´ ], [´a2´ ,´b2´ ,´c2´ ], [´a3´ ,´b3´ ,´c3´ ]]
 $winner = false
 $counter = 0
 
@@ -45,16 +45,16 @@ class Players < Game
   end
 
   def choises(player_choise)
-    coordinates_array = player_choise.upcase.split("")
+    coordinates_array = player_choise.upcase.split(´)
     col = coordinates_array[1].to_i
-    if coordinates_array.include?("A") && !$board[1][col].include?("X" || "O")
+    if coordinates_array.include?(´A´) && !$board[1][col].match?(/X|O/)
       record(1, col)
-    elsif coordinates_array.include?("B") && !$board[2][col].include?("X" || "O")
+    elsif coordinates_array.include?(´B´) && !$board[2][col].match?(/X|O/)
       record(2, col)
-    elsif coordinates_array.include?("C") && !$board[3][col].include?("X" || "O")
+    elsif coordinates_array.include?(´C´) && !$board[3][col].match?(/X|O/)
       record(3, col)
     else
-      puts "Choose again: "
+      puts ´Choose again: ´
       player_choise = gets.chomp
       choises(player_choise)
     end
@@ -69,42 +69,43 @@ class Players < Game
 
   def turns(player)
     Game.display
-    puts "#{player.name}'s turn. Choose wisely (e.g. B2)"
-    choise = gets.chomp
-    choises(choise)
+    choose()
     winner() 
-    $winner ? break : $counter += 1
+    $winner ? return : $counter += 1
     Game.tie
   end
 
+  def choose
+    puts ´#{self.name}'s turn. Choose wisely (e.g. B2)´
+    choise = gets.chomp
+    choise.size == 2 ? choises(choise) : choose()
+  end
+
   def winner
+    i = 0
     # Three in a row
     $tracker.each { |row| 
-      if row == ["#{self.token}", "#{self.token}", "#{self.token}"]
+      if row == [´#{self.token}´, ´#{self.token}´, ´#{self.token}´]
         $winner = true
-        puts "#{self.name} won!"
-      end 
+        puts ´#{self.name} won R!´
+      end
     }
     # Three in a col
-    i = 0
     for j in (0..2)
       if $tracker[i][j] == $tracker[i+1][j] && $tracker[i+1][j] == $tracker[i+2][j]
         $winner = true
-        puts "#{self.name} won!"
+        puts ´#{self.name} won!´
       end
     end
     # Diagonal
     if $tracker[i][i] == $tracker[i+1][i+1] && $tracker[i+1][i+1] == $tracker[i+2][i+2]
       $winner = true
-      puts "#{self.name} won!"
-    end
-    
-    if $tracker[i+2][i+2] == $tracker[i+1][i+1] && $tracker[i+1][i+1] == $tracker[i][i]
+      puts ´#{self.name} won!´    
+    elsif $tracker[i][i+2] == $tracker[i+1][i+1] && $tracker[i+1][i+1] == $tracker[i+2][i]
       $winner = true
-      puts "#{self.name} won!"
+      puts ´#{self.name} won!´
     end
   end
 end
 
 Game.start
-
