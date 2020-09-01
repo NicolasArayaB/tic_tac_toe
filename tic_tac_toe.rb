8833f3c7d3f7961f18438529b5f5ca3ee380c0df
NicolasArayaB <nicolasaraya@gmail.com>
2020-09-01 09:17:12 -0400

    working

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,11 +1,19 @@
 $board = [["   ", "1", "  2", "  3"], ["A ", "[ ]", "[ ]", "[ ]"], 
          ["B ", "[ ]", "[ ]", "[ ]"], ["C ", "[ ]", "[ ]", "[ ]"]]
-$tracker = [["" ,"" ,""], ["" ,"" ,""], ["" ,"" ,""]]
+$tracker = [["a1" ,"b1" ,"c1" ], ["a2" ,"b2" ,"c2" ], ["a3" ,"b3" ,"c3" ]]
 $winner = false
+$counter = 0
 
 class Game
   def self.display
     display = $board.each { |block| puts block.join }
+  end
+
+  def self.tie
+    if $counter == 9
+      $winner = true
+      puts "It's a tie!"
+    end
   end
 
   def self.start
@@ -15,21 +23,19 @@ class Game
     puts "Second player's name?: "
     second_player = gets.chomp
     player_o = Players.new(second_player, "[O]")
-    until $winner
+    until $winner == true
       player_x.turns(player_x)
-      player_o.turns(player_o)
+      $winner ? break : player_o.turns(player_o)
     end
-  end 
+  end
 end
 
 class Players < Game
-  attr_reader :token
-  
   def initialize(name, token)
     @name = name
     @token = token
   end
-
+  
   def name
     @name.capitalize
   end
@@ -40,18 +46,13 @@ class Players < Game
 
   def choises(player_choise)
     coordinates_array = player_choise.upcase.split("")
-    if coordinates_array.include?("A") && $tracker[0][coordinates_array[1].to_i - 1] == ""
-      $board[1].delete_at(coordinates_array[1].to_i)
-      $board[1].insert(coordinates_array[1].to_i, self.token)
-      $tracker[0].insert(coordinates_array[1].to_i - 1, self.token)
-    elsif coordinates_array.include?("B") && $tracker[1][coordinates_array[1].to_i - 1] == ""
-      $board[2].delete_at(coordinates_array[1].to_i)
-      $board[2].insert(coordinates_array[1].to_i, self.token)
-      $tracker[1].insert(coordinates_array[1].to_i - 1, "Taken")
-    elsif coordinates_array.include?("C") && $tracker[2][coordinates_array[1].to_i - 1] == ""
-      $board[3].delete_at(coordinates_array[1].to_i)
-      $board[3].insert(coordinates_array[1].to_i, self.token)
-      $tracker[2].insert(coordinates_array[1].to_i - 1, "Taken")
+    col = coordinates_array[1].to_i
+    if coordinates_array.include?("A") && !$board[1][col].include?("X" || "O")
+      record(1, col)
+    elsif coordinates_array.include?("B") && !$board[2][col].include?("X" || "O")
+      record(2, col)
+    elsif coordinates_array.include?("C") && !$board[3][col].include?("X" || "O")
+      record(3, col)
     else
       puts "Choose again: "
       player_choise = gets.chomp
@@ -59,14 +60,51 @@ class Players < Game
     end
   end
 
+  def record(row, col)
+    $board[row].delete_at(col)
+    $board[row].insert(col, self.token)
+    $tracker[row-1].delete_at(col-1)
+    $tracker[row-1].insert(col-1, self.token)
+  end
+
   def turns(player)
     Game.display
     puts "#{player.name}'s turn. Choose wisely (e.g. B2)"
     choise = gets.chomp
     choises(choise)
-    Game.display
+    winner() 
+    $winner ? break : $counter += 1
+    Game.tie
+  end
+
+  def winner
+    # Three in a row
+    $tracker.each { |row| 
+      if row == ["#{self.token}", "#{self.token}", "#{self.token}"]
+        $winner = true
+        puts "#{self.name} won!"
+      end 
+    }
+    # Three in a col
+    i = 0
+    for j in (0..2)
+      if $tracker[i][j] == $tracker[i+1][j] && $tracker[i+1][j] == $tracker[i+2][j]
+        $winner = true
+        puts "#{self.name} won!"
+      end
+    end
+    # Diagonal
+    if $tracker[i][i] == $tracker[i+1][i+1] && $tracker[i+1][i+1] == $tracker[i+2][i+2]
+      $winner = true
+      puts "#{self.name} won!"
+    end
+    
+    if $tracker[i+2][i+2] == $tracker[i+1][i+1] && $tracker[i+1][i+1] == $tracker[i][i]
+      $winner = true
+      puts "#{self.name} won!"
+    end
   end
 end
 
 Game.start
-p $tracker
+
